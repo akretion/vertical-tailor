0ef70d22dc43d7d87419bb75593e0737ca35d48a
raph <raph@hpar.fr>
2015-04-10 11:37:09 +0200

    add resumeMesure directive

---
+++ www/components/mesure/resumeMesure.directive.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('starter')
+.directive('resumeMesures', [function () {
+	return { 
+		scope: { mesure:'=' },
+		template: '' +
+		'<div class="row responsive-sm" ng-repeat="m in dateResume">' +
+			'<div class="col" ng-repeat="(key, val) in m" style="border: 1px solid gray">'+
+				'{{ val }} - {{ mesure.data[val] }}'+
+			'</div>'+
+		'</div>',
+		link: function ($scope, elem, attrs) {
+			$scope.dataResume = [];
+			$scope.$watchCollection('mesure.data', function (newVal, oldVal) {
+			if (newVal)
+				$scope.dateResume = splitIn3(newVal);
+			});
+
+			function splitIn3 (a) { //split an Object into array of 3 cols
+
+				var values = Object.keys(a).filter(function (k) {
+					return a[k]; //keep only keys with value
+				}).sort(); //easier to read
+
+				return values.reduce(function r(prev, current, i) {
+					if (i % 3 == 0)
+						prev.push([]);
+					prev[prev.length -1].push(current);
+					return prev;
+				}, []);
+			}
+		}
+	};
+}]);
