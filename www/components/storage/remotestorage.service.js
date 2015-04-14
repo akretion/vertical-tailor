d8dedc1dd6dd7c646460c8d0bc27cec68db951da
Hpar ionic <raph@hpar.fr>
2015-04-14 18:36:01 +0200

    add remote test

--- www/components/storage/remotestorage.service.js
+++ www/components/storage/remotestorage.service.js
@@ -1,12 +1,22 @@
 angular.module('starter')
-.factory('remoteStorage', ['$http', function($http) {
+.factory('remoteStorage', ['$http', 'jsonRpc', '$q', function($http, jsonRpc, $q) {
 	var url = 'localhost';
 	return {
 		set: function(key, value) {
 			return $http.post('key', value);
 		},
 		get: function(key) {
-			return $http.get(url+'key');
+			var object = "";
+			return jsonRpc.call('mrp.production.workcenter.line', 'get_sync_data', ['prodoo', null, [['workcenter_id', '=', 1]], 50], {}).then(function (data) {
+				console.log('dans resovle', data);
+			}, function (reason) {
+				console.log('isLogged?', jsonRpc.isLoggedIn());
+				if (reason.title === 'session_expired') {
+					console.log('session expire, go to loggin');
+					return $q.reject();
+				}
+				return $q.reject();
+			});
 		}
 	}
 }]);
