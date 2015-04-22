64091d9cc18a83938b2f1e0ea785d59214bbee1e
Hpar ionic <raph@hpar.fr>
2015-04-22 12:56:09 +0200

    add binding to odoo in remotestorage.service and remove traces

--- www/components/storage/remotestorage.service.js
+++ www/components/storage/remotestorage.service.js
@@ -1,19 +1,20 @@
 angular.module('starter')
 .factory('remoteStorage', ['$http', 'jsonRpc', '$q', function($http, jsonRpc, $q) {
-	var url = 'localhost';
+	var keys = {
+		'orders': { domain: 'sale.order' , action: 'get_measure' },
+		'formsProducts': { domain: 'product.template', action: 'get_measurable_product' },
+		'forms': { domain: 'measure.measure', action: 'get_form'}
+	};
+
 	return {
 		set: function(key, value) {
 			return $http.post('key', value);
 		},
 		get: function(key) {
-			var object = "";
-			console.log('par la');
-			return jsonRpc.call('measure.measure', 'get_form', [], {}).then(function (data) {
-				console.log('dans resovle', data);
+			return jsonRpc.call(keys[key].domain, keys[key].action, [], {}).then(function (result) {
+				return result;
 			}, function (reason) {
-				console.log('dans reason', reason);
 				if (reason.title === 'session_expired') {
-					console.log('session expire, go to loggin');
 					return $q.reject('session_expired');
 				}
 				return $q.reject(reason);
