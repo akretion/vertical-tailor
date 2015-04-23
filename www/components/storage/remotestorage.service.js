9ff55e32f5543114bccdb9b469aaf9dac5a0ebaf
raph <raph@hpar.fr>
2015-04-23 12:23:10 +0200

    remove $state dependency from remoteStorage

--- www/components/storage/remotestorage.service.js
+++ www/components/storage/remotestorage.service.js
@@ -1,29 +1,25 @@
 angular.module('starter')
-.factory('remoteStorage', ['$http', 'jsonRpc', '$q', '$state', function($http, jsonRpc, $q, $state) {
+.factory('remoteStorage', ['$http', 'jsonRpc', '$q', function($http, jsonRpc, $q) {
 	var keys = {
 		'orders': { domain: 'sale.order' , action: 'get_measure' },
 		'formsProducts': { domain: 'product.template', action: 'get_measurable_product' },
 		'forms': { domain: 'measure.measure', action: 'get_form'}
 	};
-	var global = $state.current.data.global;
 
 	return {
 		set: function(key, value) {
 			return $http.post('key', value);
 		},
 		get: function(key) {
-			if (global.isLoggedIn === false) {
+			if (!jsonRpc.isLoggedIn()) {
 				return $q.reject('session_expired');
 			}
 			return jsonRpc.call(keys[key].domain, keys[key].action, [], {}).then(function (result) {
-				global.isLoggedIn = true;
 				return result;
 			}, function (reason) {
 				if (reason.title === 'session_expired') {
-					global.isLoggedIn = false;
 					return $q.reject('session_expired');
-				} else
-					global.isLoggedIn = undefined;
+				}
 				return $q.reject(reason);
 			});
 		}
