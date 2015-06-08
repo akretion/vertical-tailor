761da1a4ce38352e0968b111109d107e5673c265
Sébastien BEAU <sebastien.beau@akretion.com>
2015-06-08 09:25:42 +0200

    [REF] clean code, pep8 and xml cleaenup

--- sale_measure/partner_measure.py
+++ sale_measure/partner_measure.py
@@ -1,5 +1,4 @@
 from openerp import fields, models
-from collections import defaultdict
 
 
 class PartnerMeasure(models.Model):
@@ -17,20 +16,4 @@ class PartnerMeasure(models.Model):
     head_measurement = fields.Float("Head Measurement")
     Leg_length = fields.Float("Leg length")
     crotch = fields.Float("Crotch")
-    pelvis_tower = fields.Flaot("Tower Pelvis ")
-
-
-def _prepare_dict(self, field):
-    return {
-        'name':#field_name,
-        'label':#field_string,
-        'widget': 'numeric'
-    }
-
-
-def _get_firm(self):
-    form = []
-    dict_fields = defaultdict(list)
-    for fields in self.get_attributes:
-        dict_fields['question'].append(self._prepare_dict(fields))
-    return dict_fields
+    pelvis_tower = fields.Flaot("Tower Pelvis")
