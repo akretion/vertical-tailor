23e34669e804140050583b81170222cf5ef0e92f
raph <raph@hpar.fr>
2015-07-31 16:48:01 +0200

    refactor settings

--- www/components/settings/settings.controller.js
+++ www/components/settings/settings.controller.js
@@ -18,9 +18,17 @@ angular.module('starter')
     $scope.login = function () {
         console.log("dans login()");
         $scope.error = "";
+        if (settings.odooServer)
+            jsonRpc.odoo_server = settings.odooServer;
+        if (settings.odooDb)
+            jsonRpc.odoo_db = settings.odooDb;
+        
         jsonRpc.login(jsonRpc.odoo_db || 'db', this.bucheUsername, this.buchePassword).then(function () { //this.buche because of new scope of ion-view
             console.log("login succeed");
             $scope.global.isLoggedIn = true;
+
+            localStorage.set('settings', settings);
+       
         }, function (reason) {
             console.log('login failed');
             console.log(reason);
@@ -40,21 +48,11 @@ angular.module('starter')
         $scope.global.isLoggedIn = false;
     };
 
-    $scope.saveSettings = function (settings) {
-        console.log('save settings', settings);
-        localStorage.set('settings', settings);
-        if (settings.odooServer)
-            jsonRpc.odoo_server = settings.odooServer;
-        if (settings.odooDb)
-            jsonRpc.odoo_db = settings.odooDb;
-    };
-
-    $scope.loadForm = function () {
-        Formulaire.load();
-    };
 
     $scope.reset = function () { 
         ['settings', 'forms', 'orders', 'formsProducts'].map(function(key) { localStorage.remove(key) });
         localStorage.set('orders', []);
+        Formulaire.load();
+
     };
 }]);
