2f2adfb47f3bb67af9647a417476c906a3a9f267
raph <raph@hpar.fr>
2015-07-31 16:49:36 +0200

    land directly in order instead of settings

--- www/app/app.js
+++ www/app/app.js
@@ -85,6 +85,5 @@ angular.module('starter', ['ionic','buche','odoo'])
     })
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/settings');
-
+    $urlRouterProvider.otherwise('/tab/order');
 }]);
