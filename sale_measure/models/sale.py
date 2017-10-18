6ba0cf51f8cd88a86e3e31e11c1c4a9aa9507522
Magno <magno.costa@akretion.com.br>
2017-10-18 14:30:20 -0200

    [10.0][MIG] Change license header and module version to new standard.

--- sale_measure/models/sale.py
+++ sale_measure/models/sale.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-Today Akretion
+# @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 """
 Sale line Class there is a link with measure Class
 
