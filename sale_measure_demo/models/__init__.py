398484e4dbb473f537a27a02961a2413388a29d5
Magno <magno.costa@akretion.com.br>
2017-10-25 11:58:47 -0200

    [10.0][MIG] Move python file to folder models.

--- sale_measure_demo/models/__init__.py
+++ sale_measure_demo/models/__init__.py
@@ -3,5 +3,4 @@
 # @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
-
+from . import measure_demo
