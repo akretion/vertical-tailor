5a1726284ea658863647997b657504365c7651d4
Magno <magno.costa@akretion.com.br>
2017-10-25 11:38:52 -0200

    [10.0][MIG] Update license header.

--- sale_measure_demo/__init__.py
+++ sale_measure_demo/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#  licence AGPL version 3 or later
-#  see license in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
-#  Copyright (C) 2015 Akretion (http://www.akretion.com).
-#  @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
-#
-##############################################################################
+# Copyright (C) 2015-Today Akretion
+# @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import measure_demo
