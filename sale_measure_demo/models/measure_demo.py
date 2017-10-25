bfa3261814fc1979407667fd11e37b1846111ab3
Magno <magno.costa@akretion.com.br>
2017-10-25 12:11:04 -0200

    [10.0][MIG] Update import .

--- sale_measure_demo/models/measure_demo.py
+++ sale_measure_demo/models/measure_demo.py
@@ -3,7 +3,7 @@
 # @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MeasureDemo(models.Model):
