003748a1364cc832c0f7c91e38dccb4e762d801c
David Beal <david.beal@akretion.com>
2017-11-03 09:22:54 +0100

    REMove sale_measure_demo dirty module

--- sale_measure_demo/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2015-Today Akretion
-# @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import models
-
