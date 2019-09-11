501ac602a0032e6c0d56da0b1a319745a9ebb561
David Beal <david.beal@akretion.com>
2019-09-11 15:46:25 +0200

    Update __manifest__.py

--- sale_measure/__manifest__.py
+++ sale_measure/__manifest__.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Sale measure",
+    'description': "Sale measure for tailor",
     'website': "http://www.akretion.com",
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
